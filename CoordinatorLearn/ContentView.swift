088d6ffe5c1689dfaef28afa60447b891e039490
MQamarInam <qamarinam028@gmail.com>
2025-12-08 22:37:51 +0500

    Initial Commit

---
+++ CoordinatorLearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoordinatorLearn
+//
+//  Created by Qaim's Macbook  on 08/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoordinatorLearn/CoordinatorLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorLearnApp.swift
+//  CoordinatorLearn
+//
+//  Created by Qaim's Macbook  on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
